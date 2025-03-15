883e1bdc99f22ca979f80c20f483bae69b43e549
Masami_husband <jeevsong@gmail.com>
2025-03-15 22:16:14 +0800

    feat: Menu 界面编写

--- src/main.ts
+++ src/main.ts
@@ -9,22 +9,25 @@ import { Game, type Types } from "phaser";
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Types.Core.GameConfig = {
-    type: Phaser.AUTO,
-    width: 1024,
-    height: 768,
-    parent: 'game-container',
-    backgroundColor: '#028af8',
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
+  type: Phaser.AUTO,   // 自动选择最佳的渲染方式：WebGL、Canvas
+  width: 1024,
+  height: 768,
+  parent: 'game-container',
+  backgroundColor: '#028af8',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  dom: {
+    createContainer: true
+  },
+  scene: [
+    Boot,
+    Preloader,
+    MainMenu,
+    MainGame,
+    GameOver
+  ]
 };
 
 export default new Game(config);
--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -1,32 +1,52 @@
 import { Scene, GameObjects } from 'phaser';
 
-export class MainMenu extends Scene
-{
-    background: GameObjects.Image;
-    logo: GameObjects.Image;
-    title: GameObjects.Text;
+export class MainMenu extends Scene {
+  background: GameObjects.Image;
+  logo: GameObjects.Image;
+  title: GameObjects.DOMElement;
+  startText: GameObjects.Text;
 
-    constructor ()
-    {
-        super('MainMenu');
-    }
+  constructor() {
+    super('MainMenu');
+  }
 
-    create ()
-    {
-        this.background = this.add.image(512, 384, 'background');
+  create() {
+    this.background = this.add.image(512, 384, 'background');
 
-        this.logo = this.add.image(512, 300, 'logo');
+    // this.logo = this.add.image(512, 300, 'logo');
+    // 添加标题 "西游记"
+    const titleHtml = `
+      <div class="rotating-text">
+        斗战西游
+      </div>
+    `;
+    this.title = this.add.dom(512, 300).createFromHTML(titleHtml).setOrigin(0.5);
 
-        this.title = this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
 
-        this.input.once('pointerdown', () => {
 
-            this.scene.start('Game');
 
-        });
-    }
+    this.startText = this.add.text(512, 480, '开始游戏', {
+      fontFamily: 'Arial Black',
+      fontSize: 38,
+      stroke: '#000000',
+      strokeThickness: 8,
+      color: '#FFFFFF',
+      align: 'center'
+    }).setOrigin(0.5);
+
+    this.startText.setInteractive();
+    this.startText.on('pointerover', () => {
+      this.startText.setScale(1.1);
+      this.startText.setColor('#FFD799');
+      this.startText.setStyle({ cursor: 'pointer' });
+    });
+    this.startText.on('pointerout', () => {
+      this.startText.setScale(1);
+      this.startText.setColor('#FFFFFF');
+      this.startText.setStyle({ cursor: 'default' });
+    });
+    this.startText.on('pointerdown', () => {
+      this.scene.start('Game');
+    });
+  }
 }
